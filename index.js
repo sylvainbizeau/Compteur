844bc3c7ca9e8b1a491af223752ce00a2db90c83
sylvainbizeau <80413243+sylvainbizeau@users.noreply.github.com>
2021-03-10 18:58:08 +0100

    test2

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-var http = require('http');
+
+// importation
+var express = require('express')
+
+//instance serveur
+var server = express();
+
+// Varriable
 var counter = 0;
-function f(req, response)
-{
-  if(req.url == '/favicon.ico') return;
-  response.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
-  counter++;
-  response.write(counter.toString()) ;
-  response.end();
-}
-var server = http.createServer(f);
-server.listen(80);
+
+//config racine
+server.get('/', function(req, res) {
+    //if(req.url == '/favicon.ico') return;
+    //res.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
+    counter++;
+    //res.write(counter);
+    //res.end();
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send(counter.toString())
+});
+
+//lancer serveur
+server.listen(1000, function(){
+    console.log('serveur en écoute');
+});
