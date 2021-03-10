e42ed1fb95213d88c5eb9e2a17b1a8e6aaa23a0d
sylvainbizeau <80413243+sylvainbizeau@users.noreply.github.com>
2021-03-10 17:33:56 +0100

    test

---
+++ index.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var counter = 0;
+function f(req, response)
+{
+  if(req.url == '/favicon.ico') return;
+  response.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
+  counter++;
+  response.write(counter.toString()) ;
+  response.end();
+}
+var server = http.createServer(f);
+server.listen(1000);
