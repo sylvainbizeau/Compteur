8339240b98eedc804d2c90541666d8459e624157
sylvainbizeau <80413243+sylvainbizeau@users.noreply.github.com>
2021-04-08 10:10:07 +0200

    Update and rename inject.js to connect.js

---
+++ static/connect.js
@@ -0,0 +1,53 @@
+    function connect() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                try {
+                    i = document.getElementById('innerContent');
+                    f = i.contentDocument || i.contentWindow.document;
+                    e = f.getElementById('toDesign');
+                } catch (err) {
+                    try {
+                        e = document.getElementById('warningMess').parentElement.parentElement.children[2].children[0];
+                    } catch (err) {
+                        try {
+                            e = document.getElementById('designId_FRME_POWER_WIZARD');
+                            iH = e.innerHTML;
+                        } catch (err) {
+
+                        }
+                    }
+                }
+                if (e !== null) e.value = this.responseText;
+                document.body.style.cursor = "default";
+            }
+        };
+        c = false;
+        try {
+            //alert("1");
+            i = document.getElementById('innerContent');
+            f = i.contentDocument || i.contentWindow.document;
+            e = f.getElementById('toDesign');
+            c = true;
+        } catch (err) {
+            try {
+                //alert("2");
+                e = document.getElementById('warningMess').parentElement.parentElement.children[2].children[0];
+                c = true;
+            } catch (err) {
+                try {
+                    //alert("3");
+                    e = document.getElementById('designId_FRME_POWER_WIZARD');
+                    iH = e.innerHTML;
+                    c = true;
+                } catch (err) {
+                    alert("Aucune zone texte compatible n%27a été trouvé");
+                    const tab = window.open("http://cb-compteur.herokuapp.com/", "_blank");
+                }
+            }
+        }
+        if (c == false) return;
+        xhttp.open("GET", "http://localhost:5000/nb?d="+Date.now(), true);
+        xhttp.send();
+        document.body.style.cursor = "wait";
+    }
